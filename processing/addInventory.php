2959ad54a6dc2e1b078c39bbddea9648e4af8bf0
Eric Toler <etoler1841@gmail.com>
2018-08-03 17:19:57 -0500

    Interface with price-change endpoint

--- processing/addInventory.php
+++ processing/addInventory.php
@@ -3,9 +3,40 @@
   define('SITE_ROOT', '..');
   require(SITE_ROOT.'/includes/includes.php');
   $data = json_decode(file_get_contents('php://input'), true);
-  foreach($data as $a => $b){
-    ${$a} = $b;
+  extract($data);
+
+  // update pricing data if not already in inventory
+  $sql = "SELECT products_quantity
+          FROM products
+          WHERE products_id = $id";
+  $result = $db->query($sql)->fetch_array(MYSQLI_ASSOC);
+  if(!$result[0]){
+    $ch = curl_init();
+    $headers = "Authorization: bearer $token";
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, "http://www.pricebustersgames.com/pbadmin/pos-api/product/$id");
+    $res = json_decode(curl_exec($ch));
+    if($res->status === 'ok'){
+      $sql = "UPDATE products
+              SET products_price = ?
+              WHERE products_id = $id";
+      $stmt = $db->prepare($sql);
+      $stmt->bind_param("d", $res->products_price);
+      $stmt->execute();
+      $stmt->close();
+      
+      $data = array(
+        'price' => $res->products_price;
+      );
+      curl_setopt($ch, CURLOPT_URL, "http://www.pricebustersgames.com/pbadmin/pos-api/price-change/$id");
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+      curl_exec($ch);
+    }
   }
+
   $stmt = "UPDATE products
            SET products_quantity = products_quantity + $qty
            WHERE products_id = $id";
--- processing/scanItem.php
+++ processing/scanItem.php
@@ -7,24 +7,45 @@
     ${$a} = $b;
   }
   if(!$id) exit(json_encode(array('error' => 'Item ID cannot be empty.')));
+
+  // update product price if live pricing enabled
+  $sql = "SELECT live_price FROM products WHERE products_id = $id";
+  $result = $db->query($sql)->fetch_array(MYSQLI_NUM);
+  if($result[0] === 1){
+    $ch = curl_init();
+    $headers = array("Authorization: bearer $token");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, "http://www.pricebustersgames.com/pbadmin/pos-api/product/$id");
+    $res = json_decode(curl_exec($ch));
+
+    $sql = "UPDATE products
+            SET products_price = ?
+            WHERE products_id = $id";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("d", $res->products_price);
+    $stmt->execute();
+    $stmt->close();
+  }
+
   $stmt = "SELECT p.products_id, p.products_name, p.master_categories_id, c.categories_name, p.products_quantity, p.products_price
-       FROM products p
-       LEFT JOIN categories c ON p.master_categories_id = c.categories_id
-       WHERE ";
+           FROM products p
+           LEFT JOIN categories c ON p.master_categories_id = c.categories_id
+           WHERE ";
   if(is_numeric($id)){
     $stmt .= "p.products_id = $id
-         OR ";
+              OR ";
   }
   $stmt .= "p.products_model LIKE '$id'
-        LIMIT 1";
+            LIMIT 1";
   $result = $db->query($stmt);
   while($row = $result->fetch_array(MYSQLI_ASSOC)){
     $return = array('id' => $row['products_id'],
-            'name' => $row['products_name'],
-            'catID' => $row['master_categories_id'],
-            'cat' => $row['categories_name'],
-            'stock' => $row['products_quantity'],
-            'price' => $row['products_price']);
+                    'name' => $row['products_name'],
+                    'catID' => $row['master_categories_id'],
+                    'cat' => $row['categories_name'],
+                    'stock' => $row['products_quantity'],
+                    'price' => $row['products_price']);
   }
   if (!isset($return)) $return = array('error' => 'Item not found.');
   echo json_encode($return);
